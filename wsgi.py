b087e7b0f2ee9c7e14a4eca19f55dad0bfae6900
Jenia Georgieva <jenia.95@abv.bg>
2018-02-15 19:19:42 +0200

    another api

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,24 @@
-# from flask import Flask
-# from flask_restful import Resource, Api
-
-# app = Flask(__name__)
-# api = Api(app)
-
-# class HelloWorld(Resource):
-#     def get(self):
-#         return {'hello': 'world'}
-
-# api.add_resource(HelloWorld, '/')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask
-application = Flask(__name__)
+from flask_restful import Resource, Api
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+api = Api(app)
 
-if __name__ == "__main__":
-    application.run()
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# from flask import Flask
+# application = Flask(__name__)
+
+# @application.route("/")
+# def hello():
+#     return "Hello World!"
+
+# if __name__ == "__main__":
+#     application.run()
