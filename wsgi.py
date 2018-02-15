a28b6a083b4cf5f28f75194ace1038bd66de1b98
Vasil Pashov <vasil.pashov1@gmail.com>
2018-02-16 00:21:56 +0200

    test db

--- wsgi.py
+++ wsgi.py
@@ -1,42 +1,8 @@
-# from flask import Flask
-# from flask import jsonify
-# # from flask_restful import Resource, Api
-
-# app = Flask(__name__)
-# # api = Api(app)
-
-
-
-# @app.route('/', methods=['GET'])
-# def get():
-#     data = {'bbb': 'aaa'}
-#     return jsonify(data)
-   
-# @app.route('/task', methods=['GET'])
-# def getNew():
-#     data = {'hesmllo': 'new'}
-#     return jsonify(data)
-
-
-# # api.add_resource(HelloWorld, '/')
-
-# if __name__ == '__main__':
-#     app.run()
-
-# # from flask import Flask
-# # application = Flask(__name__)
-
-# # @application.route("/")
-# # def hello():
-# #     return "Hello World!"
-
-# # if __name__ == "__main__":
-# #     application.run()
-
-
-
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+import os
+from pymongo import MongoClient
+import json
 
 application = Flask(__name__)
 api = Api(application)
@@ -51,6 +17,7 @@ TODOS = {
 def abort_if_todo_doesnt_exist(todo_id):
     if todo_id not in TODOS:
         abort(404, message="Todo {} doesn't exist".format(todo_id))
+
 
 parser = reqparse.RequestParser()
 parser.add_argument('task')
@@ -79,7 +46,12 @@ class Todo(Resource):
 # shows a list of all todos, and lets you POST to add new tasks
 class TodoList(Resource):
     def get(self):
-        return TODOS
+        client = MongoClient()
+        db_name = os.getenv("MONGODB_DATABASE")
+        db = client[db_name]
+        db.authenticate("admin", os.getenv("MONGODB_ADMIN_PASSWORD"))
+        return str("")
+        # return str(json.dumps({'results': list(result)}, default=json_util.default))
 
     def post(self):
         args = parser.parse_args()
@@ -88,12 +60,13 @@ class TodoList(Resource):
         TODOS[todo_id] = {'task': args['task']}
         return TODOS[todo_id], 201
 
+
 ##
-## Actually setup the Api resource routing here
+# Actually setup the Api resource routing here
 ##
 api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<todo_id>')
 
 
 if __name__ == '__main__':
-    application.run() 
+    application.run()
