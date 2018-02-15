23419f7e0cb46928a9fe5acd2772e8af8873e54f
Jenia Georgieva <jenia.95@abv.bg>
2018-02-15 20:19:25 +0200

    second method

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,20 @@
 from flask import Flask
-from flask_restful import Resource, Api
+# from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
+
 
 class HelloWorld(Resource):
+    @application.route("/")
+    def get(self):
+        return {'hello': 'world'}
+    
+    @application.route("/smth")
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+# api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run()
