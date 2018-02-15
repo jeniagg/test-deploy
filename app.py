ecc021dfea1754883f16bbac1fcd2cce8fe3b7ec
Jenia Georgieva <jenia.95@abv.bg>
2018-02-15 17:49:04 +0200

    test diff code

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
+# from flask import Flask
+# from flask_restful import Resource, Api
+
+# app = Flask(__name__)
+# api = Api(app)
+
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+
+# api.add_resource(HelloWorld, '/')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 from flask import Flask
-from flask_restful import Resource, Api
+application = Flask(__name__)
 
-app = Flask(__name__)
-api = Api(app)
+@application.route("/")
+def hello():
+    return "Hello World!"
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    application.run()
